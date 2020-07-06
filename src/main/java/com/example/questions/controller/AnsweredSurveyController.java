171955c4bf61f2a3abcaeb9963b1896a89c8abd2
X <cangozpinar@gmail.com>
2020-07-06 10:22:15 +0300

    Added SecurtiyConfig class and @PreAuthorize methods

--- src/main/java/com/example/questions/controller/AnsweredSurveyController.java
+++ src/main/java/com/example/questions/controller/AnsweredSurveyController.java
@@ -3,6 +3,7 @@ package com.example.questions.controller;
 import com.example.questions.model.AnsweredSurveyModel;
 import com.example.questions.service.AnsweredSurveyService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.view.RedirectView;
 
@@ -17,6 +18,7 @@ public class AnsweredSurveyController {
 
     //Handles post operation of answerSurvey form
     @PostMapping("/saveSurveyResults")
+    @PreAuthorize("hasAnyRole('ROLE_ADMIN','ROLE_SURVEYCREATOR')")
     public RedirectView saveSurveyResults(@RequestParam Map<String,String> questions){
         answeredSurveyService.answeredSurveyModel(questions);
         return new RedirectView("saveSurveyResults.html");
@@ -25,6 +27,7 @@ public class AnsweredSurveyController {
     //to get all of the AnsweredSurveyModel's that are in the db
     @GetMapping("/getAllAnsweredSurveys")
     @ResponseBody
+    @PreAuthorize("hasAnyRole('ROLE_ADMIN','ROLE_SURVEYCREATOR')")
     public List<AnsweredSurveyModel> getAllSurvey(){
         return answeredSurveyService.getAllAnsweredSurveys();
     }
--- src/main/java/com/example/questions/security/SecurityConfig.java
+++ src/main/java/com/example/questions/security/SecurityConfig.java
@@ -1,4 +1,28 @@
 package com.example.questions.security;
 
-public class SecurityConfig extends {
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers("/","/answerSurvey.htm").permitAll()
+                .antMatchers("/questions.html").hasAnyRole("ROLE_ADMIN", "ROLE_SURVEYCREATOR")
+                .antMatchers("/createSurvey.html").hasAnyRole("ROLE_ADMIN", "ROLE_SURVEYCREATOR")
+                .antMatchers("/answerSurveyHome.html").hasAnyRole("ROLE_ADMIN", "ROLE_SURVEYCREATOR")
+                .antMatchers("/saveSurveyResults.html").hasAnyRole("ROLE_ADMIN", "ROLE_SURVEYCREATOR")
+                .and()
+                .formLogin();
+
+
+    }
 }
